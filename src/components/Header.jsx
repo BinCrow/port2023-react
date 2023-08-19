927abea3821a717e6e9a6de3886d2c6233917197
yongbinkwon <92711444+BinCrow@users.noreply.github.com>
2023-08-19 23:14:40 +0900

    리액트 헤더완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useState } from "react"
+
+const headerNav = [
+  {
+    title: "intro",
+    url: "#intro"
+  },
+  {
+    title: "Skill",
+    url: "#Skill"
+  },
+  {
+    title: "Site",
+    url: "#Site"
+  },
+  {
+    title: "port",
+    url: "#port"
+  },
+  {
+    title: "contact",
+    url: "#contact"
+  },
+  {
+    title: "footer",
+    url: "#footer"
+  },
+];
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  }
+
   return (
-    <div>Header</div>
+    <header id="header" role="banner">
+      <div className="header__inner">
+        <div className="header__logo">
+          <h1>
+            <a href="/">portpolio<em>react.js</em></a>
+          </h1>
+        </div>
+        <nav 
+        className={`header__nav ${show ? "show" : ""}`}
+        role="navigation"
+        aria-label="메인메뉴">
+          <ul>
+            {headerNav.map((nav, key) => (
+              <li key={key}>
+                <a href={nav.url}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div
+          className="header__nav__mobile"
+          id="headerToggle"
+          aria-controls="primary-menu"
+          aria-expanded={show ? "true" : "false"}
+          role="button"
+          tabIndex="0"
+          onClick={toggleMenu}
+        >
+          <span></span>
+        </div>
+      </div>
+    </header>
   )
 }
 
